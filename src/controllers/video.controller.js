5b86d6ff05c95f796f368ffc86c65176788c2c75
ReesvGupta <guptareesav16@gmail.com>
2024-01-28 11:19:15 +0530

    add dashboard controller and update video controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -9,6 +9,73 @@ import uploadOnCloudinary from "../utils/cloudinary.js"
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
     //TODO: get all videos based on query, sort, pagination
+    const pipeLine = []
+    if (query) {
+        pipeLine.push({
+            $search: {
+                index: "search-videos",
+                text: {
+                    query: query,
+                    path: ["title", "description"],
+                },
+            },
+        })
+    }
+    if (userId) {
+        if (!isValidObjectId(userId)) {
+            throw new ApiError(400, "Provide a valid userID")
+        }
+    }
+    pipeLine.push({
+        $match: {
+            owner: new mongoose.Types.ObjectId(userId),
+        },
+    })
+    pipeLine.push({
+        $match: {
+            isPublished: true,
+        },
+    })
+    if (sortBy && sortType) {
+        pipeLine.push({
+            $sort: {
+                [sortBy]: sortType === "asc" ? 1 : -1,
+            },
+        })
+    } else {
+        pipeLine.push({ $sort: { createdAt: -1 } })
+    }
+    pipeLine.push(
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner-details",
+                pipeLine: [
+                    {
+                        $project: {
+                            username: 1,
+                            avatar: 1,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $unwind: "$owner-details",
+        }
+    )
+    const videoAggregate = await Video.aggregate(pipeLine)
+
+    const options = {
+        page: parseInt(page, 10),
+        limit: parseInt(limit, 10),
+    }
+    const videos = await Video.aggregatePaginate(options, videoAggregate)
+    return res
+        .status(200)
+        .json(new ApiResponse(200, videos, "Videos fetched sucessfully"))
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
@@ -52,13 +119,111 @@ const publishAVideo = asyncHandler(async (req, res) => {
 const getVideoById = asyncHandler(async (req, res) => {
     //TODO: get video by id
     const { videoId } = req.params
-    const video = await Video.findById(videoId)
-    if (!video || !isValidObjectId(videoId)) {
-        throw new ApiError(404, "video not found")
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Provide a valid object Id")
     }
-    return res
-        .status(200)
-        .json(new ApiResponse(200, video, "video fetched successfully"))
+    const video = await Video.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(videoId),
+            },
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner-details",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "subscriptions",
+                            localField: "_id",
+                            foreignField: "channel",
+                            as: "subscribers",
+                        },
+                    },
+                    {
+                        $addFields: {
+                            subscriberCount: {
+                                $size: "$subscribers",
+                            },
+                            isSubscribed: {
+                                $cond: {
+                                    if: {
+                                        $in: [
+                                            req.user._id,
+                                            "$subscribers.subscriber",
+                                        ],
+                                    },
+                                    then: true,
+                                    else: false,
+                                },
+                            },
+                        },
+                    },
+                    {
+                        $project: {
+                            username: 1,
+                            avatar: 1,
+                            subscriberCount: 1,
+                            isSubscribed: 1,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "video",
+                as: "likes",
+            },
+        },
+        {
+            $addFields: {
+                likeCount: {
+                    $size: "$likes",
+                },
+                isLiked: {
+                    $cond: {
+                        if: { $in: [req.user._id, "$likes.likedBy"] },
+                        then: true,
+                        else: false,
+                    },
+                },
+            },
+        },
+        {
+            $project: {
+                videoFile: 1,
+                title: 1,
+                description: 1,
+                views: 1,
+                createdAt: 1,
+                duration: 1,
+                comments: 1,
+                owner: 1,
+                likesCount: 1,
+                isLiked: 1,
+            },
+        },
+    ])
+    if (!video) {
+        throw new ApiError(400, "Failed to fetch video")
+    }
+    await Video.findByIdAndUpdate(videoId, {
+        $inc: {
+            views: 1,
+        },
+    })
+    await User.findByIdAndUpdate(req.user._id, {
+        $addToSet: {
+            watchHistory: videoId,
+        },
+    })
+    return res.status(200).json(200, video, "Sucessfully fetched the video")
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
