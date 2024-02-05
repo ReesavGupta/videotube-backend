55f9cfed155e06da6d79e36021ab9ab7735501e2
ReesvGupta <guptareesav16@gmail.com>
2024-02-05 18:32:53 +0530

    fix bugs while testing subscrition controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -49,22 +49,18 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params
+    let { subscriberId } = req.params
 
     if (!isValidObjectId(subscriberId)) {
-        throw new ApiError(400, "provide a vlaid channel ID")
+        throw new ApiError(400, "Invalid subscriberId")
     }
 
-    const channel = await User.findById(subscriberId)
+    subscriberId = new mongoose.Types.ObjectId(subscriberId)
 
-    if (!channel) {
-        throw new ApiError(404, "Channel not found")
-    }
-
-    const subscriptions = await Subscription.aggregate([
+    const subscribers = await Subscription.aggregate([
         {
             $match: {
-                channel: new mongoose.Types.ObjectId(subscriberId),
+                channel: subscriberId,
             },
         },
         {
@@ -72,38 +68,66 @@ const getUserChannelSubscribers = asyncHandler(async (req, res) => {
                 from: "users",
                 localField: "subscriber",
                 foreignField: "_id",
-                as: "subscriberList",
+                as: "subscriber",
                 pipeline: [
                     {
-                        $project: {
-                            username: 1,
-                            avatar: 1,
-                            email: 1,
+                        $lookup: {
+                            from: "subscriptions",
+                            localField: "_id",
+                            foreignField: "channel",
+                            as: "subscribedToSubscriber",
+                        },
+                    },
+                    {
+                        $addFields: {
+                            subscribedToSubscriber: {
+                                $cond: {
+                                    if: {
+                                        $in: [
+                                            subscriberId,
+                                            "$subscribedToSubscriber.subscriber",
+                                        ],
+                                    },
+                                    then: true,
+                                    else: false,
+                                },
+                            },
+                            subscribersCount: {
+                                $size: "$subscribedToSubscriber",
+                            },
                         },
                     },
                 ],
             },
         },
         {
-            $addFields: {
-                subscriberList: {
-                    $first: "$subscriberList",
+            $unwind: "$subscriber",
+        },
+        {
+            $project: {
+                _id: 0,
+                subscriber: {
+                    _id: 1,
+                    username: 1,
+                    fullName: 1,
+                    avatar: 1,
+                    subscribedToSubscriber: 1,
+                    subscribersCount: 1,
                 },
             },
         },
     ])
-    console.log(subscriptions)
-    return res.status(200).json(
-        new ApiResponse(
-            200,
-            {
-                subscriberList: subscriptions[0]?.subscriberList || [],
-            },
-            "subscriber fetched sucessfully"
-        )
-    )
-})
 
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                subscribers,
+                "subscribers fetched successfully"
+            )
+        )
+})
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { channelId } = req.params
@@ -116,6 +140,7 @@ const getSubscribedChannels = asyncHandler(async (req, res) => {
     if (!subscriber) {
         throw new error(400, "user not found.")
     }
+
     const subscriptions = await Subscription.aggregate([
         {
             $match: {
@@ -125,35 +150,61 @@ const getSubscribedChannels = asyncHandler(async (req, res) => {
         {
             $lookup: {
                 from: "users",
-                localField: "subscriber",
+                localField: "channel",
                 foreignField: "_id",
-                as: "channelSubscribedToList",
+                as: "subscribedChannel",
                 pipeline: [
                     {
-                        $project: {
-                            
-                            name: 1,
-                            email: 1,
-                            avatar: 1,
+                        $lookup: {
+                            from: "videos",
+                            localField: "_id",
+                            foreignField: "owner",
+                            as: "videos",
+                        },
+                    },
+                    {
+                        $addFields: {
+                            latestVideo: {
+                                $last: "$videos",
+                            },
                         },
                     },
                 ],
             },
         },
         {
-            $addFields: {
-                channelSubscribedToList: {
-                    $first: "$channelSubscribedToList",
+            $unwind: "$subscribedChannel",
+        },
+        {
+            $project: {
+                _id: 0,
+                subscribedChannel: {
+                    _id: 1,
+                    username: 1,
+                    fullName: 1,
+                    avatar: 1,
+                    latestVideo: {
+                        _id: 1,
+                        videoFile: 1,
+                        thumbnail: 1,
+                        owner: 1,
+                        title: 1,
+                        description: 1,
+                        duration: 1,
+                        createdAt: 1,
+                    },
                 },
             },
         },
     ])
+    console.log(subscriptions)
     return res
         .status(200)
         .json(
             new ApiResponse(
                 200,
-                subscriptions[0]?.channelSubscribedToList || []
+                subscriptions,
+                "Sucessfully fetched the subscribed channels"
             )
         )
 })
